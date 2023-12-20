917cc5952d046bc081533b55e7764570e305f0f4
mhmdfaried <muhamadfaried8@gmail.com>
2023-12-20 21:47:01 +0700

    Proses

--- proses/proses_login.php
+++ proses/proses_login.php
@@ -6,8 +6,7 @@ include 'koneksi.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$data = mysqli_query($conn,"SELECT * FROM user
- WHERE username='$username' AND password='$password'");
+$data = mysqli_query($conn,"SELECT * FROM user WHERE username='$username' AND password='$password'");
 
 $cek = mysqli_num_rows($data);
  
