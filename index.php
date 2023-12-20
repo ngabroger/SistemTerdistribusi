f0e8fb185917a747ab943ad0e8629d766dd940c6
ErlPrtm <teukuerlanggapratama@gmail.com>
2023-12-20 21:04:20 +0700

    Nambah Header untuk index

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
  $nama = 'Ujang';
-
+ include('bootstrap/header.php');
 ?>
 
+<h1 class="display-3">Tugas Sistem Terdistribusi</h1>
 <h3>Halo ngab <?php echo $nama ?></h3>
+
+<?php
+    include('bootstrap/footer.php');
+?>
